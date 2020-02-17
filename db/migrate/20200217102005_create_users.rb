68492a2dcdb3327a5a462d19bae2432de530aa6e
carolin.labinski <carolin.labinski@yahoo.de>
2020-02-17 16:29:41 +0100

    correction :

--- db/migrate/20200217102005_create_users.rb
+++ db/migrate/20200217102005_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.text: description
+      t.text :description
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
